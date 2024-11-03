f7ae6e9eb3016b4b3eb959948400486eb7bee2df
arshad012 <forarshad99@gmail.com>
2024-11-03 16:26:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -11,24 +11,18 @@ document.querySelector('.form-group').addEventListener('submit', (e) => {
 	const tr = document.createElement('tr');
 	const td1 = document.createElement('td');
 	td1.innerText = name;
-	td1.id = '1';
 	const td2 = document.createElement('td');
 	td2.innerText = author;
-	td2.id = '2';
 	const td3 = document.createElement('td');
 	td3.innerText = isbn;
-	td3.id = '3';
-	const button = document.createElement('button');
-	button.innerText = 'delete';
-	button.style.marginLeft = '50px';
-	button.className = 'delete';
-	button.onclick = () => {
-		td3.parentNode.remove();
+	const td4 = document.createElement('td');
+	td4.innerText = 'delete';
+	td4.className = 'delete';
+	td4.onclick = () => {
+		td4.parentNode.remove();
 	}
-	td3.append(button);
-	// const td4 = document.createElement('td');
 	
-	tr.append(td1, td2, td3);
+	tr.append(td1, td2, td3, td4);
 
 	book_list.append(tr);
 });
