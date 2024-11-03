9ca5fd1a0a5d59219cd097e67413ed3e24a94ab6
arshad012 <forarshad99@gmail.com>
2024-11-03 16:01:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 //your JS code here. If required.
-document.querySelector('#submit').addEventListener('click', () => {
-	// e.preventDefault();
+document.querySelector('.form-group').addEventListener('submit', (e) => {
+	e.preventDefault();
 	const book_list = document.getElementById('book-list');
 
-	// const form = document.querySelector('.form-group');
-	const name = document.getElementById('title').value;
-	const author = document.getElementById('author').value;
-	const isbn = document.getElementById('isbn').value;
+	const form = document.querySelector('.form-group');
+	const name = form.title.value;
+	const author = form.author.value;
+	const isbn = form.isbn.value;
 
 	const tr = document.createElement('tr');
 	const td1 = document.createElement('td');
@@ -15,14 +15,17 @@ document.querySelector('#submit').addEventListener('click', () => {
 	td2.innerText = author;
 	const td3 = document.createElement('td');
 	td3.innerText = isbn;
-	const td4 = document.createElement('td');
-	td4.innerText = 'delete';
-	td4.className = 'delete';
-	td4.onclick = () => {
-		td4.parentNode.remove();
+	const button = document.createElement('button');
+	button.innerText = 'delete';
+	button.style.marginLeft = '50px';
+	button.className = 'delete';
+	button.onclick = () => {
+		td3.parentNode.remove();
 	}
+	td3.append(button);
+	// const td4 = document.createElement('td');
 	
-	tr.append(td1, td2, td3, td4);
+	tr.append(td1, td2, td3);
 
 	book_list.append(tr);
 });
